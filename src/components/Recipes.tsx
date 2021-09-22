ca219b1be75c54d0f3d98fd20523b88e240c2534
Marco Martins Castanho <marcomartinscastanho>
2021-09-22 17:36:31 +0100

    Added basic state to Recipes

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,8 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+type RecipeDto = {
+  id: string;
+  title: string;
+};
 
 export class Recipes extends Component {
+  state = { recipes: [] };
+
+  componentDidMount() {
+    this.setState({
+      recipes: [
+        { id: 1, title: "Chili com carne" },
+        { id: 2, title: "Pataniscas de bacalhau" },
+        { id: 3, title: "Arroz de feijão" },
+      ],
+    });
+  }
+
   render() {
-    return <h2>Receitas</h2>;
+    return (
+      <Fragment>
+        <h2>Receitas</h2>
+        <ul>
+          {this.state.recipes.map((recipe: RecipeDto) => (
+            <li key={recipe.id}>{recipe.title}</li>
+          ))}
+        </ul>
+      </Fragment>
+    );
   }
 }
 
