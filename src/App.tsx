85fc8ebf7c38e451d30165d955b09e5fbabc890b
Marco Martins Castanho <marcomartinscastanho>
2021-09-22 16:45:46 +0100

    Added AppFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import AppFooter from "./AppFooter";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <div>
+          <h1>Hello, world!</h1>
+        </div>
+        <AppFooter />
       </div>
     );
   }
---
+++ src/AppFooter.tsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+
+export default class AppFooter extends Component {
+  render() {
+    return (
+      <Fragment>
+        <hr />
+        <p>Feito com &hearts; por Marco Martins Castanho</p>
+      </Fragment>
+    );
+  }
+}
