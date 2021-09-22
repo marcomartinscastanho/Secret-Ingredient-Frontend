f88a57bd94c420c16397f25b52454ea6dc4be130
Marco Martins Castanho <marcomartinscastanho>
2021-09-22 17:13:34 +0100

    Added bootstrap and created App navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
-import React, { Component } from "react";
-import AppFooter from "./AppFooter";
+import React, { Fragment } from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <h1>Hello, world!</h1>
+export function App() {
+  return (
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Ingrediente Secreto</h1>
+          <hr className="mb-3"></hr>
         </div>
-        <AppFooter />
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Início</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/recipes">Receitas</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Gerir Catálogo</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/recipes">
+                <Recipes />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Início</h2>;
+}
+
+function Recipes() {
+  return <h2>Receitas</h2>;
+}
+
+function Admin() {
+  return <h2>Gerir Catálogo</h2>;
 }
 
 export default App;
